41f0edcb01c4060c14155af9dfae9854bbab5148
nspyrou@outlook.com <nspyrou>
2025-07-30 04:20:23 +0300

    Employee And Manager

---
+++ BlazorApp/Models/EmployeeAndManager.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp.Models;
+
+public interface IIndividual
+{
+	string Name { get; set; }
+}
+
+public class Employee: IIndividual
+{
+	public string Name { get; set; }
+}
+
+public class Manager: IIndividual
+{
+	public string Name { get; set; }
+}
---
+++ BlazorApp/Objects/IndividualExtractor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp.Models;
+
+namespace BlazorApp.Objects;
+
+public class IndividualExtractor
+{
+	public string GetIndividualName(IIndividual individual)
+	{
+		return individual.Name;
+	}
+}
