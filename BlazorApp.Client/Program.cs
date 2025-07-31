f1b27651468faac6028b6b585c0889fa9130f88b
nspyrou@outlook.com <nspyrou>
2025-07-31 04:54:40 +0300

    Stage 6: Identity Server (Duente)

--- BlazorApp.Client/Program.cs
+++ BlazorApp.Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp.Client.Services;
+using Duende.Bff.Blazor.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 
@@ -8,6 +9,15 @@ builder.Services.AddSingleton(http => new HttpClient
 {
 	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+
+/* Identity Server https://github.com/DuendeSoftware/samples/blob/main/BFF/v3/BlazorWasm/Client/Program.cs */
+builder.Services
+	.AddBffBlazorClient()
+	.AddCascadingAuthenticationState();
+
+builder.Services.AddLocalApiHttpClient("backend");
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("backend"));
+/***********************************/
 
 builder.Services.AddSingleton<IRepository, CustomersService>();
 //builder.Services.AddSingleton<IJSRuntime, JSRuntime>();
--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,6 +1,7 @@
 using BlazorApp.Components;
 using BlazorApp.Data;
 using Microsoft.EntityFrameworkCore;
+using Duende.Bff.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,44 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 builder.Services.AddScoped(http => new HttpClient
 {
     BaseAddress = new Uri(builder.Configuration.GetSection("BaseAddress").Value!)
+});
+
+// Identity Server
+builder.Services.AddBff()
+    .AddServerSideSessions()
+    .AddBlazorServer();
+
+// Identity Server .. https://github.com/DuendeSoftware/samples/blob/main/BFF/v3/BlazorWasm/Server/Program.cs
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+    options.DefaultChallengeScheme = "oidc";
+    options.DefaultSignOutScheme = "oidc";
+})
+.AddCookie("cookie", options =>
+{
+    options.Cookie.Name = "__Host-BlazorApp";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+})
+.AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = "https://demo.duentesoftware.com";
+
+    options.ClientId = "interactive.confidential";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+    options.ResponseMode = "query";
+
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("api");
+    options.Scope.Add("offline_access");
+
+    options.MapInboundClaims = false;
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.SaveTokens = true;
+    options.DisableTelemetry = true;
 });
 
 var app = builder.Build();
@@ -37,7 +76,24 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.UseStaticFiles();
+
+/* Middlewares */
+app.UseBlazorFrameworkFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.UseBff();
+app.UseAuthorization();
+app.MapBffManagementEndpoints();
+/**************/
+
 app.UseAntiforgery();
+
+/* Middlewares */
+app.MapControllers()
+    .RequireAuthorization()
+    .AsBffApiEndpoint();
+/***************/
+
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
